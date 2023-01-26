b3501f99387fac6314ad58915237a5dd45625d01
Brian P <parmeleeb@gmail.com>
2023-01-26 15:10:53 -0600

    finished the popup and implemented function stubs

--- src/app/warehouse/warehouse.component.ts
+++ src/app/warehouse/warehouse.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Item } from '../models/item.model';
+import { Location } from '../models/location.model';
 import { Warehouse } from '../models/warehouse.model';
 import { WarehousesService } from '../services/warehouses.service';
 
@@ -12,20 +13,82 @@ export class WarehouseComponent {
 
   formStyle: string = '';
   selectedItem = new Item(77,"george", "dsadasd", 23, 1);
+  itemToEdit: number = -1;
+  warehouse: Warehouse = new Warehouse(-1, "", new Location("","","",-1), -1, []);
+  ItemToUpdate: Item = this.selectedItem;
+  itemName: string = '';
+  itemDescription: string = '';
+  itemQuantity: number = -1;
+  itemSize: number = -1;
+
+
+  constructor(private warehouseService: WarehousesService) {
+    let possibleWarehouse = warehouseService.getWarehouseById(warehouseService.warehouseToDisplay);
+    if (possibleWarehouse)
+      this.warehouse = possibleWarehouse;
+  }
 
 
   openItemPopup(item: Item) {
-    this.formStyle = 'item-popup-background'
-    this.selectedItem = item
-    
+    this.formStyle = "item-popup-background";
+    this.selectedItem = item;
+
   }
 
-  closePopup(){
-    this.formStyle = "item-popup-hidden "
+  closeItemPopup(){
+    this.formStyle = "item-popup-hidden";
+    this.setToEdit(-1);
   }
 
-  warehouse: Warehouse | null;
-  constructor(private warehouseService: WarehousesService) {
-    this.warehouse = warehouseService.getWarehouseById(warehouseService.warehouseToDisplay);
+  isOpenForEdit(){
+    return this.selectedItem.getItemId() == this.itemToEdit;
+  }
+
+  setToEdit(id: number){
+    this.itemToEdit = id;
+    this.ItemToUpdate = this.selectedItem;
+    if(id == -1) {
+      this.resetValues();
+    } else {
+      this.setValues();
+    }
+  }
+
+  resetValues() {
+    this.itemName = '';
+    this.itemDescription = '';
+    this.itemQuantity = -1;
+    this.itemSize = -1;
+  }
+
+  setValues() {
+    this.itemName = this.ItemToUpdate.getItemName();
+    this.itemDescription = this.ItemToUpdate.getItemDescription();
+    this.itemQuantity = this.ItemToUpdate.getItemCount();
+    this.itemSize = this.ItemToUpdate.getItemSize();
+  }
+
+  submitChanges() {
+    if (confirm("Overwrite current item with new values?")) {
+      console.log(this.ItemToUpdate);
+      this.setUpdatedProperties();
+      this.warehouseService.updateItem(this.warehouse.getId(), this.selectedItem.getItemId(), this.ItemToUpdate);
+      console.log(this.ItemToUpdate);
+      this.setToEdit(-1);
+    }
+  }
+
+  setUpdatedProperties() {
+    this.ItemToUpdate.setItemName(this.itemName);
+    this.ItemToUpdate.setItemDescription(this.itemDescription);
+    this.ItemToUpdate.setItemCount(this.itemQuantity);
+    this.ItemToUpdate.setItemSize(Number(this.itemSize));
+  }
+
+  confirmDelete() {
+    if (confirm("This item will be PERMANENTLY deleted.")) {
+      this.warehouseService.deleteItem(this.warehouse.getId(), this.selectedItem.getItemId());
+      this.closeItemPopup();
+    }
   }
 }
